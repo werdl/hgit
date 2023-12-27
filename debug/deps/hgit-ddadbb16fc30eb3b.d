44e5522c0606975b73ebf940f04e00232841f512
werdl <werdl_is_cool@outlook.com>
2023-12-27 17:28:09 +0000

    "right from cli"

--- debug/deps/hgit-ddadbb16fc30eb3b.d
+++
@@ -1,9 +0,0 @@
-/mnt/c/Users/Dell/coding/hgit/./debug/deps/hgit-ddadbb16fc30eb3b: src/main.rs src/callee.rs src/cmd.rs
-
-/mnt/c/Users/Dell/coding/hgit/./debug/deps/hgit-ddadbb16fc30eb3b.d: src/main.rs src/callee.rs src/cmd.rs
-
-src/main.rs:
-src/callee.rs:
-src/cmd.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
--- debug/hgit.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Dell/coding/hgit/./debug/hgit: /mnt/c/Users/Dell/coding/hgit/./src/callee.rs /mnt/c/Users/Dell/coding/hgit/./src/cmd.rs /mnt/c/Users/Dell/coding/hgit/./src/main.rs
