470c0fcd5b9b8b98f60099586c7f01a98e6826da
werdl <werdl_is_cool@outlook.com>
2023-12-28 08:07:29 +0000

    add start command

--- README.md
+++ README.md
@@ -4,22 +4,28 @@
 ## Commands
 - By default, your arguments are sent straight to the `git` executable (which must be installed on your system)
 - There are also of course some HGit only commands
-### `get [provider] repo`
+## `get [provider] repo`
 - clones repo from provider
 - supports github (`--github | -g`) and gitlab (`--gitlab | -l`)
-#### Examples
+### Examples
 `hgit get werdl/hgit` - default pulls from github
 
 `hgit get -l inkscape/inkscape` - from gitlab
 
 `hgit get -g -l --github spartanproj/os` - last flag takes priority
 
-### `go [commit message]`
+## `go [commit message]`
 - performs these operations, after concatenating all future arguments to one for `commit -m`
 ```bash
 add .
 commit -m <concat_message>
 push
 ```
-#### Examples
+### Examples
 `hgit go my first commit` - creates and pushes commit by name of `my first commit`
+
+## `template [options] <template_addr>`
+- creates a new repo, after cloning template_addr (does that in the same way `get` does, with provider flags)
+- options must include `--name=<your_name>`
+### Examples
+`hgit start --name=cc werdl/c_project`
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod cmd;
 
 use std::env;
 use std::process::exit;
+use std::process::Command;
 
 use callee::*;
 
@@ -50,6 +51,52 @@ fn main() {
             args_git.append(&mut new_args);
 
             call_git(args_git);
+        }
+
+        "template" => {
+            /*
+                * create a new repository, from template
+                * templates are pulled from any repository
+            */
+            let mut provider = "github".to_string();
+
+            let mut parser = cmd::start(Some(bad_arg));
+            parser.add_option('g', "github", true);
+            parser.add_option('l', "gitlab", true);
+            parser.add_option('n', "name", false);
+            let trimmed = args.get(1..).unwrap().to_vec();
+            let result = parser.parse(trimmed.clone(), 0);
+
+            if !result.contains_key("name") {
+                println!("Please specify a name!");
+                exit(-1);
+            }
+            
+            for (k, _v) in result.iter() {
+                if vec!["gitlab", "github"].contains(&k.as_str()) {
+                    provider = k.to_string();
+                }
+            }
+
+            let mut args_git: Vec<String> = Vec::new();
+            args_git.push("clone".to_string());
+            let mut new_args = remove_flags(args.get(2..).unwrap().to_vec());
+
+            let name = result["name"].clone();
+
+            new_args[0] = format!("http://www.{}.com/{}.git", provider, new_args[0]);
+            args_git.append(&mut new_args);
+            args_git.push(result["name"].clone()); // clone into given subdir
+
+            println!("{:?}", args_git);
+            call_git(args_git); // clone into a new directory
+
+            Command::new("rm")
+                .args(vec!["-rf", format!("./{}/.git", name).as_str()])
+                .status()
+                .expect("Failed to execute command");
+
+            call_str(format!("init {0} --template={0}", name));            
         }
 
         "go" => {
